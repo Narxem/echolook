315a821566502e08f4e332067bf8f06a313b1a8e
AGEZ <adrien.agez@pictime.com>
2017-04-07 15:49:38 +0200

    Random finished

--- src/AppBundle/Controller/RandomLookController.php
+++ src/AppBundle/Controller/RandomLookController.php
@@ -2,15 +2,35 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\RandomLook\RandomLookCreator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GraphAware\Neo4j\Client\Client;
 
 class RandomLookController extends Controller
 {
+    protected $_randomLookCreator;
+
+    public function __construct() {
+        $this->_randomLookCreator = new RandomLookCreator();
+
+    }
 
 
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        return $this->render(
+            'default/index.html.twig',
+            $this->_randomLookCreator->findRandomClothes());
 
     }
+
+
+    /**
+     * @return \GraphAware\Neo4j\Client\Client
+     */
+    private function getNeo4jClient()
+    {
+        return $this->get('neo4j.client');
+    }
+
 }
--- src/AppBundle/RandomLook/RandomLookCreator.php
+++ src/AppBundle/RandomLook/RandomLookCreator.php
@@ -8,8 +8,36 @@
 
 namespace AppBundle\RandomLook;
 
+use GraphAware\Neo4j\Client\Client;
+use GraphAware\Neo4j\Client\ClientBuilder;
 
 class RandomLookCreator
 {
+
+    protected $_client;
+
+    public function __construct() {
+        $this->_client = ClientBuilder::create()
+            ->addConnection('default', "http://neo4j:pictime@localhost:7474")
+            ->build();
+    }
+
+    function findRandomClothes() {
+        $query = 'MATCH (n:Image {type:"ACCESSOIRES"}) RETURN n ORDER BY rand() LIMIT 1';
+        $clothes = [];
+        $result = $this->_client->run('MATCH (n:Image {type:"ACCESSOIRES"}) RETURN n ORDER BY rand() LIMIT 1');
+        $clothes["ACCESSOIRES"] = $result->firstRecord()->get('n')->values()['path'];
+        $result = $this->_client->run('MATCH (n:Image {type:"HAUTS"}) RETURN n ORDER BY rand() LIMIT 1');
+        $clothes["HAUTS"] = $result->firstRecord()->get('n')->values()['path'];
+        $result = $this->_client->run('MATCH (n:Image {type:"BAS"}) RETURN n ORDER BY rand() LIMIT 1');
+        $clothes["BAS"] = $result->firstRecord()->get('n')->values()['path'];
+        $result = $this->_client->run('MATCH (n:Image {type:"CHAUSSURES"}) RETURN n ORDER BY rand() LIMIT 1');
+        $clothes["CHAUSSURES"] = $result->firstRecord()->get('n')->values()['path'];
+        $result = $this->_client->run('MATCH (n:Image {type:"MANTEAUX"}) RETURN n ORDER BY rand() LIMIT 1');
+        $clothes["MANTEAUX"] = $result->firstRecord()->get('n')->values()['path'];
+
+
+        return $clothes;
+    }
 
 }
