981b30117ecb019ae2bdb63f6490be8f8fada0e7
Sarah <sarah.wissocq@pictime.com>
2017-04-07 14:45:54 +0200

    Modif web

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig', array("hauts" => "","bas" =>"", "manteaux" =>"", "chaussures" => "", "accessoires" => ""));
+        return $this->render('default/index.html.twig', array("HAUTS" => "","BAS" =>"", "MANTEAUX" =>"", "CHAUSSURES" => "", "ACCESSOIRES" => ""));
     }
 }
